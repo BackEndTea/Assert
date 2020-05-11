0c7618c57d5c9effd66f914a9e24d1da55a3b516
peter279k <peter279k@gmail.com>
2020-05-11 16:59:43 +0800

    Add initialized Type::array and Type::object tests

--- test/TypeTest.php
+++ test/TypeTest.php
@@ -81,4 +81,59 @@ final class TypeTest extends TestCase
         $this->expectException(InvalidArgumentException::class);
         $call($input);
     }
+
+    /**
+     * @param mixed $array
+     *
+     * @dataProvider \BackEndTea\Assert\Test\TypeProvider::array()
+     */
+    public function testArray($array): void
+    {
+        $this->assertSame($array, Type::array()($array));
+    }
+
+    /**
+     * @param mixed $input
+     *
+     * @dataProvider \BackEndTea\Assert\Test\TypeProvider::int()
+     * @dataProvider \BackEndTea\Assert\Test\TypeProvider::string()
+     * @dataProvider \BackEndTea\Assert\Test\TypeProvider::null()
+     * @dataProvider \BackEndTea\Assert\Test\TypeProvider::float()
+     * @dataProvider \BackEndTea\Assert\Test\TypeProvider::bool()
+     */
+    public function testNotArray($input): void
+    {
+        $call = Type::array();
+
+        $this->expectException(InvalidArgumentException::class);
+        $call($input);
+    }
+
+    /**
+     * @param mixed $object
+     *
+     * @dataProvider \BackEndTea\Assert\Test\TypeProvider::object()
+     */
+    public function testObject($object): void
+    {
+        $this->assertSame($object, Type::object()($object));
+    }
+
+    /**
+     * @param mixed $input
+     *
+     * @dataProvider \BackEndTea\Assert\Test\TypeProvider::int()
+     * @dataProvider \BackEndTea\Assert\Test\TypeProvider::string()
+     * @dataProvider \BackEndTea\Assert\Test\TypeProvider::null()
+     * @dataProvider \BackEndTea\Assert\Test\TypeProvider::float()
+     * @dataProvider \BackEndTea\Assert\Test\TypeProvider::bool()
+     * @dataProvider \BackEndTea\Assert\Test\TypeProvider::array()
+     */
+    public function testNotObject($input): void
+    {
+        $call = Type::object();
+
+        $this->expectException(InvalidArgumentException::class);
+        $call($input);
+    }
 }
